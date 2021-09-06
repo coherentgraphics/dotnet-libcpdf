a9b555f5928fea84ec409eefa1e6d33a484e39c5
John Whitington <john@coherentgraphics.co.uk>
2021-09-06 15:00:40 +0200

    First interop

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace dotnet_libcpdf
 {
     class Program
     {
+        [DllImport("libcpdf.so")]
+        static extern void cpdf_startup(IntPtr[] ptr);
+        [DllImport("libcpdf.so")]
+        static extern IntPtr cpdf_version();
+        [DllImport("libcpdf.so")]
+        static extern void cpdf_setFast();
+        [DllImport("libcpdf.so")]
+        static extern void cpdf_setSlow();
         static void Main(string[] args)
         {
+            IntPtr[] camlargs = {};
+            cpdf_startup(camlargs);
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Marshal.PtrToStringAuto(cpdf_version()));
         }
     }
 }
