d24011e20e529e88d520d669da71738878cfe423
John Whitington <john@coherentgraphics.co.uk>
2021-09-06 15:35:39 +0200

    more

--- Program.cs
+++ Program.cs
@@ -5,20 +5,26 @@ namespace dotnet_libcpdf
 {
     class Program
     {
-        [DllImport("libcpdf.so")]
-        static extern void cpdf_startup(IntPtr[] ptr);
-        [DllImport("libcpdf.so")]
-        static extern IntPtr cpdf_version();
-        [DllImport("libcpdf.so")]
-        static extern void cpdf_setFast();
-        [DllImport("libcpdf.so")]
-        static extern void cpdf_setSlow();
+        /* CHAPTER 0. Preliminaries */
+        [DllImport("libcpdf.so")] static extern void cpdf_startup(IntPtr[] ptr);
+        [DllImport("libcpdf.so")] static extern IntPtr cpdf_version();
+        [DllImport("libcpdf.so")] static extern void cpdf_setFast();
+        [DllImport("libcpdf.so")] static extern void cpdf_setSlow();
+        [DllImport("libcpdf.so")] static extern int cpdf_lastError();
+        [DllImport("libcpdf.so")] static extern IntPtr cpdf_lastErrorString();
+        [DllImport("libcpdf.so")] static extern void cpdf_clearError();
+        [DllImport("libcpdf.so")] static extern void cpdf_onExit();
+
         static void Main(string[] args)
         {
             IntPtr[] camlargs = {};
             cpdf_startup(camlargs);
-            Console.WriteLine("Hello World!");
             Console.WriteLine(Marshal.PtrToStringAuto(cpdf_version()));
+            cpdf_setSlow();
+            cpdf_setFast();
+            //Console.WriteLine("lastError = %i\n", cpdf_lastError());
+            //Console.WriteLine("lastErrorString = %s\n", Marshal.PtrToStringAuto(cpdf_lastErrorString()));
+            cpdf_onExit();
         }
     }
 }
