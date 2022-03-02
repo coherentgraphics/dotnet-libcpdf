c18dfb1344d92f8fdc696f35b1832a7438873638
John Whitington <john@coherentgraphics.co.uk>
2022-03-02 16:57:33 +0000

    more

--- splitter.ml
+++ splitter.ml
@@ -1,47 +1,14 @@
 (* Split pandoc HTML output into chapters *)
 open Soup
 
-let soup = read_file "ocamlfromtheverybeginning.html" |> parse
+let soup = read_file "bin/Debug/net6.0/osx-arm64/dotnet-libcpdf.xml" |> parse
 
-let body_contents = soup $$ "body" |> R.first |> children
-
-let filenum = ref 0
-
-let open_a_file () =
-  let name = Printf.sprintf "html/split%02i.html" !filenum in
-    (*Printf.printf "Writing %s\n%!" name;*)
-    open_out_bin name
-
-let the_open_file = ref (open_a_file ())
-
-let contents_of_file filename =
-  let ch = open_in_bin filename in
-  let s = really_input_string ch (in_channel_length ch) in
-  close_in ch;
-  s
-
-let header = contents_of_file "html/header.html"
-let footer = contents_of_file "html/footer.html"
-
-let copy s = output_string !the_open_file s
+let body_contents = soup $$ "doc" |> R.first |> children
 
 let () =
-  copy header;
   iter
     (fun node ->
-       if 
-         match element node with
-         | Some e -> name e = "h1"
-         | _  -> false
-       then
-         begin
-           filenum := !filenum + 1;
-           copy footer;
-           close_out !the_open_file;
-           the_open_file := open_a_file ();
-           copy header
-         end;
-       copy (to_string node))
+       match element node with
+         e -> Printf.printf "%s\n" (to_string node)
+    )
     body_contents;
-    copy footer;
-    close_out !the_open_file
