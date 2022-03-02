c694a483362d86d08fd492778930947820af4455
John Whitington <john@coherentgraphics.co.uk>
2022-03-02 17:49:48 +0000

    more

--- splitter.ml
+++ splitter.ml
@@ -1,14 +1,17 @@
-(* Split pandoc HTML output into chapters *)
+(* Convert .NET XML Comments file to plain text. *)
 open Soup
 
 let soup = read_file "bin/Debug/net6.0/osx-arm64/dotnet-libcpdf.xml" |> parse
 
-let body_contents = soup $$ "doc" |> R.first |> children
+let body_contents = soup $$ "member"
 
 let () =
   iter
     (fun node ->
        match element node with
-         e -> Printf.printf "%s\n" (to_string node)
+         e ->
+           Printf.printf "%s\n\n%s\n\n"
+           (R.attribute "name" node)
+           (match leaf_text (node $ "summary") with Some s -> s | None -> "")
     )
     body_contents;
